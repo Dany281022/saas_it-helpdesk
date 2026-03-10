336f8c6ffd39822663220decca64d3918bef505a
Dany <a00316024@mycambrian.ca>
2026-03-10 12:30:53 -0400

    Final build: App and API optimized

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,19 @@ import type { AppProps } from 'next/app';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+/**
+ * Composant racine de l'application Next.js.
+ * Enveloppe l'application avec ClerkProvider pour gérer l'authentification SaaS (Step 7).
+ */
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider 
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      {...pageProps}
+    >
       <Component {...pageProps} />
     </ClerkProvider>
   );
 }
+
+export default MyApp;
